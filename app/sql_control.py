da367a810e5582168fa7bac7239b38d54e58b5c6
Earthpea1983\2018 <amwuuuuje@sina.com>
2018-02-17 00:30:00 +0800

    modified as windows version

--- app/sql_control.py
+++ app/sql_control.py
@@ -5,8 +5,8 @@ class SqlControl:
 
     def __init__(self):
         self.BASE_DIR = settings.base_dir()    #Futures_Web DIR path
-        self.sfdata_db_path = self.BASE_DIR + '/database' + '/Sfdata.db'      # database path of sf data
-        self.commodity_db_path = self.BASE_DIR + '/database' + '/Commodity.db'  # database path of commodity
+        self.sfdata_db_path = self.BASE_DIR + '\\database' + '\\Sfdata.db'      # database path of sf data
+        self.commodity_db_path = self.BASE_DIR + '\\database' + '\\Commodity.db'  # database path of commodity
 
     def open_sf_conn(self):
         self.sf_conn = sqlite3.connect(self.sfdata_db_path)   #conn of sfdata open
